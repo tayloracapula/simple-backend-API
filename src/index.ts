effaf2d28bec8f77ffb3482e5f380cc12903f5a8
Fred Taylor <alfred.taylor@capula.com>
2025-02-25 12:38:21 +0000

    add dotenv for environment variable management and create DbInteraction class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { Hono } from "hono";
 import { DatabaseManager } from "server/DbManagement";
-
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT; 
---
+++ src/server/DbInteraction.ts
@@ -0,0 +1,5 @@
+class DbInteraction {
+    constructor() {
+        
+    }
+}
