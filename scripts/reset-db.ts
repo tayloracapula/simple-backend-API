b3024f84746e190b64b556bee4d1ff9a71d173ac
Fred Taylor <alfred.taylor@capula.com>
2025-04-25 16:52:51 +0100

    update .gitignore to include sqlite database files and add resetdb script for database management

---
+++ scripts/reset-db.ts
@@ -0,0 +1,29 @@
+import  fs  from "fs";
+import path from "path";
+
+console.log(`
+   ╭────────────╮
+   |  ◕ ◡ ◕    |  
+   |  ╭───╮     |   Bun says BYE BYE DataBase
+   |  ╰───╯     |   I'LL MISS YOU!
+   ╰────────────╯
+
+`);
+
+const dbDir = path.join(process.cwd(), 'db');
+
+if (fs.existsSync(dbDir)) {
+    const files = fs.readdirSync(dbDir);
+    for (const file of files) {
+        if (file.endsWith('.sqlite') || 
+            file.endsWith('.sqlite-journal') ||
+            file.endsWith('.sqlite-shm') ||
+            file.endsWith('.sqlite-wal')) {
+        fs.unlinkSync(path.join(dbDir, file));
+        }
+    }
+
+    fs.rmdirSync(dbDir,{recursive:true})
+}
+
+fs.mkdirSync(dbDir,{recursive:true});
