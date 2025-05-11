cd0b147186ed1e6c4909c2ca6489bb3c19b5ff56
Fred Taylor <alfred.taylor@capula.com>
2025-05-11 15:54:44 +0100

    registered routes for user and manager

--- src/server/routes/managerRoutes.ts
+++ src/server/routes/managerRoutes.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
 import type { DataSource } from "typeorm";
 import { BaseRoute } from "./baseRoute";
+import { LeaveBookingController } from "server/controllers/LeaveBookingController";
+import { RouteRegistry } from "./RouteRegistry";
+import { ManagerLeaveGetRouteHandler } from "./RouteHandlers/Leave/ManagerSpecific/ManagerLeaveGetHandler";
+import { ManagerLeavePatchHandler } from "./RouteHandlers/Leave/ManagerSpecific/ManagerLeavePatchHandler";
 
 export class ManagerRoutes extends BaseRoute {
     getBasePath(): string {
@@ -8,7 +12,12 @@ export class ManagerRoutes extends BaseRoute {
     }
     registerRoutes(app: Hono, dataSource: DataSource): void {
         const managerGroup = new Hono()
+        const leaveBookingController = new LeaveBookingController(dataSource);
 
+        new RouteRegistry(managerGroup,dataSource)
+            .register(ManagerLeaveGetRouteHandler,leaveBookingController)
+            .register(ManagerLeavePatchHandler,leaveBookingController)
+            .registerAll();
 
         this.handle404(managerGroup);
         app.route(this.getBasePath(),managerGroup);
--- src/server/routes/userRoutes.ts
+++ src/server/routes/userRoutes.ts
@@ -1,6 +1,11 @@
 import { Hono } from "hono";
 import type { DataSource } from "typeorm";
 import { BaseRoute } from "./baseRoute";
+import { LeaveBookingController } from "server/controllers/LeaveBookingController";
+import { RouteRegistry } from "./RouteRegistry";
+import { UserLeaveGetRouteHandler } from "./RouteHandlers/Leave/UserSpecific/UserLeaveGetHandler";
+import { UserLeavePatchHandler } from "./RouteHandlers/Leave/UserSpecific/UserLeavePatchHandler";
+import { LeavePostRouteHandler } from "./RouteHandlers/Leave/LeavePostHandler";
 
 export class UserRoutes extends BaseRoute {
     getBasePath(): string {
@@ -8,7 +13,13 @@ export class UserRoutes extends BaseRoute {
     }
     registerRoutes(app: Hono, dataSource: DataSource): void {
         const userGroup = new Hono()
+        const leaveBookingController = new LeaveBookingController(dataSource);
 
+        new RouteRegistry(userGroup,dataSource)
+            .register(UserLeaveGetRouteHandler,leaveBookingController)
+            .register(UserLeavePatchHandler,leaveBookingController)
+            .register(LeavePostRouteHandler,leaveBookingController)
+            .registerAll()
 
         this.handle404(userGroup);
         app.route(this.getBasePath(),userGroup);
